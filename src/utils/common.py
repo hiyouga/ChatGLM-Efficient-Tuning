34e1a67a425bdec4135ea01f46b100e3d35bf9fa
hiyouga <hiyouga@buaa.edu.cn>
2023-06-07 16:42:50 +0800

    tiny fix

--- src/utils/common.py
+++ src/utils/common.py
@@ -97,10 +97,10 @@ def init_adapter(
         logger.info("Fine-tuning method: LoRA")
         lastest_checkpoint = None
 
-        assert os.path.exists(os.path.join(model_args.checkpoint_dir[0], CONFIG_NAME)), \
-            "The given checkpoint is not a LoRA checkpoint, please specify `--finetuning_type full/p_tuning/freeze` instead."
-
         if model_args.checkpoint_dir is not None:
+            assert os.path.exists(os.path.join(model_args.checkpoint_dir[0], CONFIG_NAME)), \
+                "The given checkpoint is not a LoRA checkpoint, please specify `--finetuning_type full/p_tuning/freeze` instead."
+
             if is_trainable and model_args.resume_lora_training: # continually train on the lora weights
                 checkpoints_to_merge, lastest_checkpoint = model_args.checkpoint_dir[:-1], model_args.checkpoint_dir[-1]
             else:
